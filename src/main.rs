c90ac1fb1220750098cfb493fc7866f778cea2f4
Simon Leigh <crumpetybumpety@gmail.com>
2019-03-26 18:28:26 +0900

    progress?

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 
 use std::io;
+use std::iter;
+use std::collections::HashMap;
 use std::net::SocketAddr;
 
 use tokio::prelude::*;
@@ -7,18 +9,38 @@ use tokio::net::UdpSocket;
 
 struct Server {
     socket: UdpSocket,
-    buf: [u8; 1500]
+    buffer: Vec<u8>,
+    connections: HashMap<SocketAddr, Connection>
 }
 
 impl Server {
 
-    fn send(&mut self, amt: usize, addr: &SocketAddr) -> Poll<usize, io::Error> {
-        let msg = [b"Ack - ", &self.buf[..amt]].concat();
-        self.socket.poll_send_to(&msg, addr)
+    fn new (addr: SocketAddr, max_packet_size: usize) -> Result<Self, io::Error> {
+        let socket = UdpSocket::bind(&addr)?;
+        let buffer: Vec<u8> = iter::repeat(0).take(max_packet_size).collect();
+        let connections = HashMap::new();
+
+        Ok(Server {
+            socket,
+            buffer,
+            connections
+        })
     }
 
-    fn read(&mut self) -> Poll<(usize, SocketAddr), io::Error> {
-        self.socket.poll_recv_from(&mut self.buf)
+    fn read(&mut self) -> Poll<(Vec<u8>, SocketAddr), io::Error> {
+        self.socket.poll_recv_from(&mut self.buffer)
+            .map(|poll| {
+                let (amt, addr) = match poll {
+                    Async::Ready((amt, addr)) => (amt, addr),
+                    Async::NotReady => return Async::NotReady
+                };
+
+                Async::Ready((self.buffer[..amt].to_vec(), addr))
+            })
+    }
+
+    fn local_addr(&self) -> Result<SocketAddr, io::Error> {
+        self.socket.local_addr()
     }
 
 }
@@ -29,7 +51,7 @@ impl Future for Server {
 
     fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
         loop {
-            let (msg_len, addr) = match self.read() {
+            let (data, addr) = match self.read() {
                 Ok(Async::Ready(t)) => t,
                 Ok(Async::NotReady) => return Ok(Async::NotReady),
                 Err(e) =>  {
@@ -38,20 +60,34 @@ impl Future for Server {
                 },
             };
 
-            self.send(msg_len, &addr).unwrap();
+            if self.connections.contains_key(&addr) {
+                for (_, conn) in self.connections.iter() {
+                    self.socket.poll_send_to(&data, conn.remote_addr()).unwrap();
+                }
+            } else {
+                if self.connections.len() < 4 {
+                    self.connections.insert(addr, Connection { addr });
+                }
+            }
 
-            println!("{}", String::from_utf8_lossy(&self.buf[0..msg_len]));
+            println!("{}", String::from_utf8_lossy(&data));
         }
     }
        
 }
 
+struct Connection {
+    addr: SocketAddr
+}
+
+impl Connection {
+    fn remote_addr(&self) -> &SocketAddr {
+        &self.addr
+    }
+}
+
 fn main() {
     let addr = "127.0.0.1:12345".parse().unwrap();
-    let socket = UdpSocket::bind(&addr).unwrap();
-
-    tokio::run(Server {
-        socket: socket,
-        buf: [0; 1500]
-    });
+    
+    tokio::run(Server::new(addr, 1500).expect("bind fail"));
 }
